b2fab3098ad71de3287a3c61d057b6827a06b98f
zhaoyingkui <qianfeng@YDZ.local>
2015-06-30 10:28:17 +0800

    asd
    ad

---
+++ demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by qianfeng on 15/6/30.
+//  Copyright (c) 2015年 ydz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  demo
+//
+//  Created by qianfeng on 15/6/30.
+//  Copyright (c) 2015年 ydz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
